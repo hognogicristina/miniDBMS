a1ce96e6fa4015a48b6c0cff366b43078e9789ab
Cristina Hognogi <90508610+hognogicristina@users.noreply.github.com>
2024-11-14 08:43:50 +0200

    fixed examples

--- server/handlers/database/indexHandler.js
+++ server/handlers/database/indexHandler.js
@@ -7,7 +7,6 @@ const {getCurrentDatabase} = require("../../db/dbState");
 const {parseCommandIndex} = require('../../utils/validators/commandValidation');
 
 async function handleCreateIndex(command, socket) {
-  // Check if a database is selected
   const dbError = checkDatabaseSelection();
   if (dbError) {
     socket.write(dbError);
@@ -15,7 +14,6 @@ async function handleCreateIndex(command, socket) {
   }
 
   const commandText = command.join(' ');
-  // Updated regex to handle multiple columns
   const regex = /create\s+(unique\s+)?index\s+(\w+)\s+on\s+(\w+)\s+([\w,\s]+)/i;
   const match = commandText.match(regex);
 
@@ -30,14 +28,12 @@ async function handleCreateIndex(command, socket) {
   const tableName = match[3];
   const columns = match[4].split(',').map(col => col.trim());
 
-  // Find the specified table
   const table = findTable(tableName);
   if (typeof table === 'string') {
     socket.write(table);
     return;
   }
 
-  // Check if all specified columns exist in the table
   for (const column of columns) {
     const columnError = checkColumnExists(table, tableName, column);
     if (columnError) {
@@ -46,20 +42,17 @@ async function handleCreateIndex(command, socket) {
     }
   }
 
-  // Check if the index already exists
   const indexError = checkExistingIndex(table, indexName);
   if (indexError) {
     socket.write(indexError);
     return;
   }
 
-  // Get the current database and collection name
   const currentDatabase = getCurrentDatabase();
   const collectionName = `${currentDatabase}_${tableName}_idx_${indexName}`;
 
   try {
     await client.db(currentDatabase).createCollection(collectionName);
-
     const indexEntry = {
       indexName: indexName,
       isUnique: isUnique ? 1 : 0,
